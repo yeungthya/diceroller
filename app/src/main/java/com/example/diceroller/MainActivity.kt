1fe9de6c9157356a4c8598cff9588748771f07eb
yeungthye <yeungthya@yahoo.com>
2019-10-18 03:00:36 +0800

    commit dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -14,12 +14,30 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice()  }
+
+        val calButton: Button = findViewById(R.id.count_button)
+        calButton.setOnClickListener { cal()  }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
+        val randomInt2 = Random().nextInt(6) + 1
+        val randomInt3 = Random().nextInt(6) + 1
 
         val resultText: TextView = findViewById(R.id.result_text)
-        resultText.text = randomInt.toString();
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        resultText.text = randomInt.toString()
+        resultText2.text = randomInt2.toString()
+        resultText3.text = randomInt3.toString()
+    }
+
+    private fun cal(){
+        val resultText: TextView = findViewById(R.id.result_text)
+        val resultText2: TextView = findViewById(R.id.result_text2)
+        val resultText3: TextView = findViewById(R.id.result_text3)
+        val total: TextView = findViewById(R.id.result);
+        val num = resultText.text.toString().toInt() + resultText2.text.toString().toInt() + resultText3.text.toString().toInt()
+        total.text = num.toString()
     }
 }
