fcf79a8d32a411004385367255a305096ac30452
yeungthye <yeungthya@yahoo.com>
2019-10-18 17:35:41 +0800

    commit dice roller

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.*
 
@@ -14,30 +15,49 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.setOnClickListener { rollDice()  }
-
-        val calButton: Button = findViewById(R.id.count_button)
-        calButton.setOnClickListener { cal()  }
     }
 
     private fun rollDice() {
         val randomInt = Random().nextInt(6) + 1
         val randomInt2 = Random().nextInt(6) + 1
         val randomInt3 = Random().nextInt(6) + 1
+        val dice1: ImageView = findViewById(R.id.dice1)
+        val dice2: ImageView = findViewById(R.id.dice2)
+        val dice3: ImageView = findViewById(R.id.dice3)
 
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        val resultText3: TextView = findViewById(R.id.result_text3)
-        resultText.text = randomInt.toString()
-        resultText2.text = randomInt2.toString()
-        resultText3.text = randomInt3.toString()
-    }
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
 
-    private fun cal(){
-        val resultText: TextView = findViewById(R.id.result_text)
-        val resultText2: TextView = findViewById(R.id.result_text2)
-        val resultText3: TextView = findViewById(R.id.result_text3)
-        val total: TextView = findViewById(R.id.result);
-        val num = resultText.text.toString().toInt() + resultText2.text.toString().toInt() + resultText3.text.toString().toInt()
+        val drawableResource2 = when (randomInt2) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        val drawableResource3 = when (randomInt3) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        dice1.setImageResource(drawableResource)
+        dice2.setImageResource(drawableResource2)
+        dice3.setImageResource(drawableResource3)
+
+        val num = randomInt + randomInt2 + randomInt3
+        val total: TextView = findViewById(R.id.total);
         total.text = num.toString()
     }
 }
